2d0506e81bcc69ec2a96a0f4c7ac51c80edbd94c
Martin Oputa <46689083+iprowriter@users.noreply.github.com>
2026-05-11 00:08:42 +0200

    Add: string-matching-in-an-array - submission-3

---
+++ Data Structures & Algorithms/string-matching-in-an-array/submission-3.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string[]} words
+     * @return {string[]}
+     */
+    stringMatching(words) {
+
+        let resultSet = new Set();
+
+        for(let i = 0; i<words.length; i++){
+            let current = words[i];
+
+            for(let j = 0; j<words.length; j++) {
+
+                if( i !== j && words[j].includes(current)){
+                    resultSet.add(current);
+                    break;
+                }
+            }
+        };
+        return Array.from(resultSet);
+    }
+}
