2afa03b0a34878ddc7738ff610def100195732b8
Martin Oputa <46689083+iprowriter@users.noreply.github.com>
2026-05-14 10:18:54 +0200

    Add: longest-strictly-increasing-or-strictly-decreasing-subarray - submission-1

---
+++ Data Structures & Algorithms/longest-strictly-increasing-or-strictly-decreasing-subarray/submission-1.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestMonotonicSubarray(nums) {
+      
+        let longestInCreasingStreak = 1;
+        let currentIncreasingStreak = 1;
+
+        let longestDecreasingStreak = 1;
+        let currentDecreasingStreak = 1;
+
+        for(let i=1; i<nums.length; i++){
+
+            if(nums[i] > nums[i-1]){
+                currentIncreasingStreak ++;
+                longestInCreasingStreak = Math.max(longestInCreasingStreak, currentIncreasingStreak);
+
+                currentDecreasingStreak = 1;
+                
+            } else if(nums[i] < nums[i-1]){
+                currentDecreasingStreak ++;
+                longestDecreasingStreak = Math.max(longestDecreasingStreak, currentDecreasingStreak);
+
+                currentIncreasingStreak = 1;
+
+            } else {
+                currentIncreasingStreak = 1;
+                currentDecreasingStreak = 1;
+            }
+
+        }
+        console.log("longestInCreasingStreak: ", longestInCreasingStreak)
+       console.log("longestDecreasingStreak: ", longestDecreasingStreak)
+        return Math.max(longestInCreasingStreak, longestDecreasingStreak)
+    }
+}
