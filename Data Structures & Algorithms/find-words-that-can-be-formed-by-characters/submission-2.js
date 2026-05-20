a81407ae03c3513f20a11dc81e3c561fed9e2c98
Martin Oputa <46689083+iprowriter@users.noreply.github.com>
2026-05-20 18:44:35 +0200

    Add: find-words-that-can-be-formed-by-characters - submission-2

---
+++ Data Structures & Algorithms/find-words-that-can-be-formed-by-characters/submission-2.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {string[]} words
+     * @param {string} chars
+     * @return {number}
+     */
+    countCharacters(words, chars) {
+
+        let totalLength = 0;
+
+        // Step 1: Count frequencies of available characters
+        const charCounts = new Array(26).fill(0);
+        for (let i = 0; i < chars.length; i++) {
+            charCounts[chars.charCodeAt(i) - 97]++;
+        }
+
+        // Step 2: Check each word
+        for (const word of words) {
+            const wordCounts = new Array(26).fill(0);
+            let canForm = true;
+
+            for (let i = 0; i < word.length; i++) {
+                const index = word.charCodeAt(i) - 97;
+                wordCounts[index]++;
+
+                // If we need more of this letter than we have available, stop
+                if (wordCounts[index] > charCounts[index]) {
+                    canForm = false;
+                    break;
+                }
+            }
+
+            if (canForm) {
+                totalLength += word.length;
+            }
+        }
+
+        return totalLength;
+    }
+}
+
+//Time Complexity: O(C + N * L)
+//Space Complexity: O(1)
