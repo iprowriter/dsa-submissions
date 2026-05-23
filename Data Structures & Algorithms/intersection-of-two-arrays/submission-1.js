3f01fd91008637a2aac8a6cf489b0c160467acfd
Martin Oputa <46689083+iprowriter@users.noreply.github.com>
2026-05-23 07:35:54 +0200

    Add: intersection-of-two-arrays - submission-1

---
+++ Data Structures & Algorithms/intersection-of-two-arrays/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     * @return {number[]}
+     */
+    intersection(nums1, nums2) {
+
+        const resultSet = new Set();
+
+        for (const num of nums1){
+            if(nums2.indexOf(num) !== -1) {
+                resultSet.add(num)
+            }
+        }
+
+        return Array.from(resultSet)
+    }
+}
