119d832f15d62453b32286ec91a22c4dc21e81c3
Martin Oputa <46689083+iprowriter@users.noreply.github.com>
2026-05-24 08:26:24 +0200

    Add: find-the-index-of-the-first-occurrence-in-a-string - submission-1

---
+++ Data Structures & Algorithms/find-the-index-of-the-first-occurrence-in-a-string/submission-1.js
@@ -0,0 +1,12 @@
+class Solution {
+    /**
+     * @param {string} haystack
+     * @param {string} needle
+     * @return {number}
+     */
+    //Cheat code: Use in-built JS methods
+    strStr(haystack, needle) {
+
+        return haystack.indexOf(needle)
+    }
+}
