4b314f8be5af334c418d6e1a39e3a4212c4a4f5c
Martin Oputa <46689083+iprowriter@users.noreply.github.com>
2026-05-26 17:27:40 +0200

    Add: merge-strings-alternately - submission-2

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-2.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} word1
+     * @param {string} word2
+     * @return {string}
+     */
+    mergeAlternately(word1, word2) {
+
+        let word1Pointer = 0;
+        let word2Pointer = 0;
+        let result = [];
+
+        while(word1Pointer < word1.length || word2Pointer < word2.length) {
+
+            if(word1Pointer < word1.length) {
+                result.push(word1[word1Pointer])
+                word1Pointer++
+            }
+
+             if(word2Pointer < word2.length) {
+                result.push(word2[word2Pointer])
+                word2Pointer++
+            }
+        }
+        return result.join("")
+    }
+}
+//Time Complexity: O(W1 + W2) (Linear Time)
+//Space Complexity: O(W1 + W2):
