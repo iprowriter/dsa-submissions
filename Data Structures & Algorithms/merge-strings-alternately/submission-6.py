caf00edfe2ae71da467ea2c4f7c8a9b4d1a760a2
Martin Oputa <46689083+iprowriter@users.noreply.github.com>
2026-05-27 17:31:31 +0200

    Add: merge-strings-alternately - submission-6

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-6.py
@@ -0,0 +1,19 @@
+#This solution was also solved in JavaScript, switch to JavaScript to see JS Solutions
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        word1Pointer = 0
+        word2Pointer = 0
+        result = []
+
+        while word1Pointer < len(word1) or word2Pointer < len(word2):
+            if word1Pointer < len(word1):
+                result.append(word1[word1Pointer])
+                word1Pointer +=1
+            
+            if word2Pointer < len(word2):
+                result.append(word2[word2Pointer])
+                word2Pointer +=1
+
+        return "".join(result)
+
+
