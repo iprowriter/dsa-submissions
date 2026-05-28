ddde86d79421bfb745adafc292379cd14f8096a5
Martin Oputa <46689083+iprowriter@users.noreply.github.com>
2026-05-28 12:30:24 +0200

    Add: move-zeroes - submission-2

---
+++ Data Structures & Algorithms/move-zeroes/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        writePointer = 0
+
+        for readPointer in range(len(nums)):
+            if nums[readPointer] != 0:
+                temp = nums[writePointer]
+                nums[writePointer] = nums[readPointer]
+                nums[readPointer] = temp
+                writePointer += 1
+        
