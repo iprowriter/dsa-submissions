8e5be66b068fae4686cf9dac7f61276419e9d57a
Martin Oputa <46689083+iprowriter@users.noreply.github.com>
2026-05-31 08:35:32 +0200

    Add: squares-of-a-sorted-array - submission-2

---
+++ Data Structures & Algorithms/squares-of-a-sorted-array/submission-2.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    sortedSquares(nums) {
+
+        let left = 0
+        let right = nums.length - 1
+        let result = new Array(nums.length).fill(0)
+        let write = nums.length - 1
+
+        while (left <= right){
+
+            if (Math.abs(nums[left]) > Math.abs(nums[right])){
+                result[write] = nums[left] * nums[left]
+                left++
+            } else {
+                result[write] = nums[right] * nums[right]
+                right--
+            };
+
+            write--
+        }
+            
+        return result
+    }
+}
+
+//Time Complexity: O(N)
+//Space Complexity: O(N)
