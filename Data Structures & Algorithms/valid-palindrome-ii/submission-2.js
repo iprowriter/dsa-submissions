905804a1c9d309a9c95e645034b3073ffec1fa5e
Martin Oputa <46689083+iprowriter@users.noreply.github.com>
2026-06-03 10:23:02 +0200

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    validPalindrome(s) {
+
+      let left = 0;
+      let right = s.length - 1;
+
+      while(left < right) {
+        if(s[left] !== s[right]) {
+            return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right -1)
+        }
+        left++
+        right--
+      }
+
+      return true;
+
+    
+
+      //Helper function to check Palindrome
+      function isPalindrome(s, l, r){
+
+        while(l < r) {
+            if(s[l] !== s[r]) return false;
+            l++
+            r--
+        };
+        return true;
+      }
+
+    }
+}
+//Time Complexity: O(N)
+//Space Complexity: O(1)
