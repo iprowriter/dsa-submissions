53178815426a0c8dc6387334566bfc3eb202cd66
Martin Oputa <46689083+iprowriter@users.noreply.github.com>
2026-06-12 07:53:57 +0200

    Add: linked-list-cycle-detection - submission-2

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-2.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head) {
+        
+        let curr = head;
+        const set = new Set();
+
+        while (curr !== null){
+            console.log("current curr: ", curr)
+            if(set.has(curr.next)){
+                return true
+            } 
+            set.add(curr.next)
+
+            let nextNode = curr.next;
+            curr = nextNode;
+        }
+        return false;
+    }
+}
+
+//Time Complexity: O(N)
+//Space Complexity: O(N)
