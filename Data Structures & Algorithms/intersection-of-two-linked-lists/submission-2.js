d567fd484fe72d201f4f1494ae5c3185057882d3
Martin Oputa <46689083+iprowriter@users.noreply.github.com>
2026-06-18 09:55:55 +0200

    Add: intersection-of-two-linked-lists - submission-2

---
+++ Data Structures & Algorithms/intersection-of-two-linked-lists/submission-2.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {ListNode} headA
+     * @param {ListNode} headB
+     * @return {ListNode}
+     */
+    //this is not optimal solution. Just bruteforce, see solution B
+    getIntersectionNode(headA, headB) {
+
+        let headACur = headA;
+        let headBCur = headB;
+
+        let comparison = 0
+
+        while(headACur !== null) {
+
+            while(headBCur !== null) {
+                comparison++
+                console.log(`Comparison ${comparison}: `, headACur.next)
+                console.log(`Comparison ${comparison}: `, headBCur.next)
+
+                if(headACur === headBCur){
+                    return headACur
+                }
+        
+              headBCur = headBCur.next
+            }
+            headBCur = headB
+            headACur = headACur.next
+        }
+
+        return null;
+    }
+}
