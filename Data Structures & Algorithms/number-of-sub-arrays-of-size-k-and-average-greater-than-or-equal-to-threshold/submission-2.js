1f671f41cb4073df5b84bc27a1dd6b8b166b288f
Martin Oputa <46689083+iprowriter@users.noreply.github.com>
2026-07-02 11:12:43 +0200

    Add: number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold - submission-2

---
+++ Data Structures & Algorithms/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/submission-2.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @param {number} k
+     * @param {number} threshold
+     * @return {number}
+     */
+    //take any combination of array
+    //each sub-array must be equal to 'k'
+    //the correct sub-array must have average greater or equal to 'threshold'
+    //average is calculated by taking the sub-array and dividing by 'k'
+    //Do I need to sort this array? not really needed
+    //brute-force solution. Check solution 2 for optimized solution
+    numOfSubarrays(arr, k, threshold) {
+
+        let subArraysCount = 0;
+
+        for(let i=0; i<=arr.length - k; i++){
+            let sub = 0;
+            for(let j=i; j< i + k; j++){
+               sub += arr[j]
+            }
+    
+            if( sub / k >= threshold){
+                subArraysCount++
+            }
+        }
+        return subArraysCount
+    }
+}
+
+//Time Complexity: O(n .k)
+//Space complexity: O(1)
